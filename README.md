7cc5608d0bd28e2f290fd34c5bf76c1c9b84d06c
Mizok <karasunohina@gmail.com>
2022-04-05 03:34:01 +0800

    fix-some-webpack-bug

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 title: webpack-playground-template
 date: 
 author: Mizok
-version: 0.1.0
+version: 0.1.1
 tags: 
 ---
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -21,11 +21,11 @@ const getEntriesByParsingTemplateNames = (templatesFolderName,atRoot = true)=>{
       entryName = entryName.replace(entryRegex, `$3`);
     }
 
-    entryName= atRoot?entryName:`${templatesFolderName}/${entryName}`
+    const entryDependency= atRoot?entryName:`${templatesFolderName}/${entryName}`
 
-    let entryPath = resolve(__dirname, `src/ts/${entryName}.ts`);
+    let entryPath = resolve(__dirname, `src/ts/${entryDependency}.ts`);
     // entry stylesheet
-    let entryStyleSheetPath = resolve(__dirname, `./src/scss/${entryName}.scss`);
+    let entryStyleSheetPath = resolve(__dirname, `./src/scss/${entryDependency}.scss`);
 
     entryPath = fs.existsSync(entryPath)?entryPath:undefined;
     entryStyleSheetPath = fs.existsSync(entryStyleSheetPath)?entryStyleSheetPath:undefined;
