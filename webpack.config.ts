781577a87870c8f621cbe43851bcf320ab13d6ba
Mizok <karasunohina@gmail.com>
2022-04-05 03:52:52 +0800

    add scss util

--- webpack.config.ts
+++ webpack.config.ts
@@ -30,8 +30,8 @@ const getEntriesByParsingTemplateNames = (templatesFolderName,atRoot = true)=>{
     entryPath = fs.existsSync(entryPath)?entryPath:undefined;
     entryStyleSheetPath = fs.existsSync(entryStyleSheetPath)?entryStyleSheetPath:undefined;
 
-    // import es6-promise automatically
-    entryObj[entryName] = ['es6-promise/auto',entryPath, entryStyleSheetPath].filter(function (x: string | undefined) {
+    // import es6-promise and scss util automatically
+    entryObj[entryName] = ['es6-promise/auto',entryPath, './src/scss/util/main.scss',entryStyleSheetPath].filter(function (x: string | undefined) {
       return x !== undefined;
     });
 
